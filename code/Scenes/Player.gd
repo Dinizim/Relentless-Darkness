ae0f0d946f148a577be2c23d26119242cee93938
Christinne <vanessa0christinne@gmail.com>
2024-01-02 02:10:52 +0100

    Player move w/ animation

---
+++ code/Scenes/Player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+	
+	match input_direction:
+		Vector2.RIGHT:
+			$Animation.play("anim_side")
+			$Animation.flip_h = false
+			velocity = Vector2(speed, 0)
+
+		Vector2.LEFT:
+			$Animation.play("anim_side")
+			$Animation.flip_h = true
+			velocity = Vector2(-speed, 0)
+
+		Vector2.DOWN:
+			$Animation.play("anim_down")
+			velocity = Vector2(0, speed)
+
+		Vector2.UP:
+			$Animation.play("anim_up")
+			velocity = Vector2(0, -speed)
+
+		Vector2.ZERO:
+			$Animation.play("anim_idle")
+			velocity = Vector2.ZERO
+		
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
