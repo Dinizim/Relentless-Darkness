d518f9cc73d3f0ae6979a23acb233a9b8363e37e
Christinne <vanessa0christinne@gmail.com>
2024-01-03 14:05:29 +0100

    Update ghost_enemy

--- code/Scripts/Ghost_Enemy.gd
+++ code/Scripts/Ghost_Enemy.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
-
-class_name Enemy
-var speed: float = 10.0
-
 @onready var target = $"../Player"
 
+func take_damage(damage):
+	Global.ghost_health -= damage
+
 func _physics_process(delta):
-	if target == null: get_tree().get_nodes_in_group("Player")[0]
+	if Global.ghost_health <= 0:
+		queue_free()
+
 	if target != null:
-		velocity = position.direction_to(target.position) * speed
+		velocity = position.direction_to(target.position) * Global.ghost_speed
 		move_and_slide()
