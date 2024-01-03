e2475b99f42886593f88ef1da24de262d45ad28c
Christinne <vanessa0christinne@gmail.com>
2024-01-03 13:37:36 +0100

    Remove func enemy

--- code/Scripts/Orb_Enemy.gd
+++ code/Scripts/Orb_Enemy.gd
@@ -3,9 +3,6 @@ extends CharacterBody2D
 @onready var _animation_player = $Animation
 var orb_health = Global.orb_health
 
-func enemy():
-	pass
-
 func _physics_process(_delta):
 	_animation_player.play("idle")
 	
