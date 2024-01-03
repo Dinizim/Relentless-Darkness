d8f7c57e5ffd948eaaa6a608dc5cf03d4394f237
Christinne <vanessa0christinne@gmail.com>
2024-01-03 14:04:50 +0100

    Add global var player_speed

--- code/Scripts/Global.gd
+++ code/Scripts/Global.gd
@@ -2,6 +2,9 @@ extends Node
 
 # player
 @export var player_health : int = 100
+@export var player_speed : int = 200
+
+# weapons
 @export var sword_damage : int = 10
 
 # enemy orb
@@ -11,4 +14,4 @@ extends Node
 # enemy ghost
 @export var ghost_health : int = 10
 @export var ghost_damage : int = 15
-@export var ghost_speed : int = 10
+@export var ghost_speed : int = 20
