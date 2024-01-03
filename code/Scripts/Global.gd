374aa86e0a3b7a98d1b1b7c209fd2fe3ed6e9fe7
Christinne <vanessa0christinne@gmail.com>
2024-01-03 13:37:25 +0100

    Add ghost attributes

--- code/Scripts/Global.gd
+++ code/Scripts/Global.gd
@@ -2,8 +2,13 @@ extends Node
 
 # player
 @export var player_health : int = 100
-@export var player_damage : int = 10
+@export var sword_damage : int = 10
 
 # enemy orb
-@export var orb_health : int = 10
+@export var orb_health : int = 5
 @export var orb_damage : int = 10
+
+# enemy ghost
+@export var ghost_health : int = 10
+@export var ghost_damage : int = 15
+@export var ghost_speed : int = 10
