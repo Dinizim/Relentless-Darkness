c2d956eab5b8aa99e6cd1a9c12a4260d8d0a848d
Nicollas Diniz <nicollasfdiniz@gmail.com>
2024-01-04 14:14:26 -0300

    repare

--- code/Scripts/Ghost_Enemy.gd
+++ code/Scripts/Ghost_Enemy.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
 
 @onready var target = $"../Player"
-func take_damage(damage):
-	Global.ghost_health -= damage
 
-func _physics_process(delta):
-	
-	if Global.ghost_health <= 0:
-		queue_free()
 
-	
-		
+func _physics_process(delta):	
 	if target != null:
 		velocity = position.direction_to(target.position) * Global.ghost_speed
 		
 		if (target.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = true
+			$Animation.flip_h = true
 		else:
-			$AnimatedSprite2D.flip_h = false
+			$Animation.flip_h = false
 		move_and_slide()
+		
+	if Global.ghost_health <= 0:
+		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		body.take_damage(Global.ghost_damage)
+		
+func take_damage(damage):
+	Global.ghost_health -= damage
