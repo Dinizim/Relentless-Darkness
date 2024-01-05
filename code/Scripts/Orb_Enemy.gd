db4a7cef83ff7ed3d4bb35375d4ab12a7c1f8916
Nicollas Diniz <nicollasfdiniz@gmail.com>
2024-01-04 22:16:06 -0300

    add Ball_Enemy

--- code/Scripts/Orb_Enemy.gd
+++ code/Scripts/Orb_Enemy.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-@onready var _animation_player = $Animation
+@onready var target = $"../Player"
+@onready var _animation_player = $Body
 
-func take_damage(damage):
-	Global.orb_health -= damage
-
-func _physics_process(_delta):
-	_animation_player.play("idle")
+func _physics_process(delta):	
+	_animation_player.play("default")		
+	if target != null:
+		velocity = position.direction_to(target.position) * Global.orb_speed
+		
+		if (target.position.x - position.x) < 0:
+			$Body.flip_h = true
+		else:
+			$Body.flip_h = false
+		move_and_slide()
 	
 	if Global.orb_health <= 0:
 		queue_free()
@@ -14,3 +20,6 @@ func _physics_process(_delta):
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		body.take_damage(Global.orb_damage)
+
+func take_damage(damage):
+	Global.orb_health -= damage
